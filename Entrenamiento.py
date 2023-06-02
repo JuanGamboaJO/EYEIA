eaf2f44553922f0cab9342c43f89de1474a5476b
JuanGamboaJO <juanjosegamboauis@gmail.com>
2023-06-02 12:27:23 -0500

    Borrando Archivos Innecesarios

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -1,3 +1,50 @@
+# from maxMin import maxAndMin
+# from convNet import ConvNet
+# import cv2 as cv
+# import torch
+# import torch.nn as nn
+# import pyautogui
+# import os
+# import numpy as np
+# import copy
+
+# device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# def dataLoad(path, want = 0):
+#     totalHolder = []
+#     dims = [1600,900]
+
+#     im = cv.cvtColor(cv.imread("eyes/1500.50.17.jpg"), cv.COLOR_BGR2GRAY)
+#     top = max([max(x) for x in im])
+#     totalHolder.append(((torch.tensor([[im]]).to(dtype=torch.float,device=device))/top,torch.tensor([[int(("1400.150.7.jpg".split("."))[want])/dims[want]]]).to(dtype=torch.float,device=device)))
+
+#     # print(totalHolder)
+#     return totalHolder
+
+# trainingSet = dataLoad("eyes")
+
+
+# def eyetrack(xshift = 30, yshift=150, frameShrink = 0.15):
+#     model= ConvNet().to(device)
+#     model.load_state_dict(torch.load("xModels/118.plt",map_location=device))
+#     model.eval()
+
+
+#     im=trainingSet
+
+#     for i,(im, label) in enumerate(trainingSet):
+
+#         output=model(im)
+#         print(output.item())
+#         print(output.item()*1600)
+ 
+
+#     # top = max([max(x) for x in im])
+#     # im2=(torch.tensor([[im]]).to(dtype=torch.float,device=device))/top
+#     # lable=float(600/1600)
+
+# eyetrack()
+
 import numpy as np
 import cv2 as cv
 import os
@@ -8,7 +55,7 @@ import torchvision
 import torch.functional as F
 import matplotlib.pyplot as plt
 from maxMin import maxAndMin
-from convNet import ConvNet
+from conv2Net import ConvNet
 from torch.optim.lr_scheduler import CyclicLR
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -33,19 +80,21 @@ def dataLoad(path, want = 0):
     # print(totalHolder)
     return totalHolder
 
-def evaluateModel(model,testSet, sidelen = 1600):
+def evaluateModel(model,testSetderecho,testsetizquierdo, sidelen = 1600):
     model.eval()
     err = 0
-    for (im, label) in testSet:
-        output = model(im)
+    for i,((im, label),(im2,label2)) in enumerate(zip(testSetderecho,testsetizquierdo)):
+        output = model(im,im2)
         err += abs(output.item() - label.item())
     model.train()
 
-    return (err/len(testSet)*sidelen)
+    return (err/len(testSetderecho)*sidelen)
 
 
-trainingSet = dataLoad("eyes")
-test = dataLoad("testeyes")
+trainingSet_Derecho = dataLoad("ojoderecho")
+trainingSet_izquierdo = dataLoad("ojoizquierdo")
+testizquierdo = dataLoad("testeyesizquierdo")
+testderecho = dataLoad("testeyesderecho")
 
 
 num_epochs = 20
@@ -57,9 +106,9 @@ def trainModel():
     # model.cuda()
 
     criterion = nn.MSELoss()
-    #optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    #scheduler = CyclicLR(optimizer, base_lr=0.001, max_lr=0.01, mode='triangular')
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    #optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    scheduler = CyclicLR(optimizer, base_lr=0.001, max_lr=0.01, mode='triangular')
 
     bestModel = model
     bestScore = 10000
@@ -69,10 +118,11 @@ def trainModel():
     model.train()
     for epoch in range(num_epochs):
         print(epoch)
-        np.random.shuffle(trainingSet)
+        np.random.shuffle(trainingSet_Derecho)
+        np.random.shuffle(trainingSet_izquierdo)
 
-        for i,(im, label) in enumerate(trainingSet):
-            output = model(im)
+        for i,((im, label),(im2,label2)) in enumerate(zip(trainingSet_Derecho,trainingSet_izquierdo)):
+            output = model(im,im2)
             #output = torch.mean(output, dim=1, keepdim=True)
             loss = criterion(output, label)
             loss.backward()
@@ -80,15 +130,15 @@ def trainModel():
 
     
             #  Ajustar la tasa de aprendizaje
-            #scheduler.step()
+            scheduler.step()
             optimizer.zero_grad()
     
             # Reiniciar los gradientes
-            if (i+1) % 1620 == 0:
+            if (i+1) % 1800 == 0:
                 # testSc = evaluateModel(model,test,sidelen=900)
-                testSc = evaluateModel(model,test)
+                testSc = evaluateModel(model,testderecho,testizquierdo)
                 # trainSc = evaluateModel(model,trainingSet,sidelen=900)
-                trainSc = evaluateModel(model,trainingSet)
+                trainSc = evaluateModel(model,trainingSet_Derecho,trainingSet_izquierdo)
                 if testSc < bestScore:
                     bestModel = copy.deepcopy(model)
                     bestScore = testSc
@@ -99,12 +149,12 @@ def trainModel():
                 print(trainSc)
                 print(testSc)
                 print ('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                       .format(epoch+1, num_epochs, i+1, len(trainingSet), loss.item()))
+                       .format(epoch+1, num_epochs, i+1, len(trainingSet_Derecho), loss.item()))
 
     bigTest.append(testscores)
     bigTrain.append(trainscores)
 
-    finalScore = evaluateModel(bestModel,test)
+    finalScore = evaluateModel(bestModel,testderecho,testizquierdo)
     # finalScore = evaluateModel(bestModel,test,sidelen=900)
     print(finalScore)
 
@@ -117,3 +167,6 @@ trainModel()
 
 
 
+
+   
+    
